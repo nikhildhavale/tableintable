8ea891aab1e93078d99d66580eb0e382109ac3b8
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2023-11-23 13:56:50 +0530

    Better approach stack view :)

---
+++ TestTableView/TableStackView.swift
@@ -0,0 +1,20 @@
+//
+//  TableStackView.swift
+//  TestTableView
+//
+//  Created by Nikhil Dhavale on 23/11/23.
+//
+
+import UIKit
+
+class TableStackView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
